68d7d85bed807f3fe27e30f1b4f08183bd84cadc
Andriikryl <andreyk557@gmail.com>
2023-04-04 20:16:51 +0300

    git[init]

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Popular from "./components/Popular";
+
+function App() {
+  return (
+    <div className="App">
+      <Popular />
+    </div>
+  );
+}
+
+export default App;

5a753a3593ccd81074a965a54283771a1e8eea21
Andriikryl <andreyk557@gmail.com>
2023-04-06 13:31:31 +0300

    feat[rout-dom-anime-item]

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Popular from "./components/Popular";
+import AnimeItem from "./components/AnimeItem";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Popular />
-      </div>
+      <Routes>
+        <Route path="/" element={<Popular />}></Route>
+        <Route path="/anime/:id" element={<AnimeItem />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }

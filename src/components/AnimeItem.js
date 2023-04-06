31a79b1bb2899ca457865a20eb97cc6001c12c4d
Andriikryl <andreyk557@gmail.com>
2023-04-06 15:50:45 +0300

    feat[anime-items-styles]

--- src/components/AnimeItem.js
+++ src/components/AnimeItem.js
@@ -34,93 +34,110 @@ function AnimeItem() {
     const response = await fetch(`https://api.jikan.moe/v4/anime/${anime}`);
     const data = await response.json();
     setAnime(data.data);
+  };
+
+  // get character
+
+  const getCharacters = async (anime) => {
+    const response = await fetch(
+      `https://api.jikan.moe/v4/anime/${anime}/characters`
+    );
+    const data = await response.json();
+    setCharacters(data.data);
     console.log(data.data);
   };
 
   useEffect(() => {
     getAnime(id);
+    getCharacters(id);
   }, []);
 
   return (
-    <div>
-      <h1>{title}</h1>
-      <div className="details">
-        <div className="detail">
-          <div className="image">
-            <img
-              src={images?.jpg.large_image_url}
-              alt="Title-image of this anime"
-            ></img>
-          </div>
-          <div className="anime-details">
-            <p>
-              <span>Aired: </span>
-              <span>{aired?.string}</span>
-            </p>
-            <p>
-              <span>Rating: </span>
-              <span>{rating}</span>
-            </p>
-            <p>
-              <span>Rank: </span>
-              <span>{rank}</span>
-            </p>
-            <p>
-              <span>Scored by: </span>
-              <span>{score}</span>
-            </p>
-            <p>
-              <span>Popularity: </span>
-              <span>{popularity}</span>
-            </p>
-            <p>
-              <span>Status: </span>
-              <span>{status}</span>
-            </p>
-            <p>
-              <span>Source: </span>
-              <span>{source}</span>
-            </p>
-            <p>
-              <span>Season: </span>
-              <span>{season}</span>
-            </p>
-            <p>
-              <span>Duration: </span>
-              <span>{duration}</span>
-            </p>
-          </div>
-        </div>
-        <div>
-          <p className="description">
-            {showMore ? synopsis : synopsis?.substring(0, 200) + "..."}
-          </p>
-          <button
-            onClick={() => {
-              setShowMore(!showMore);
-            }}
-          >
-            {showMore ? "Close" : "reade more"}
-          </button>
-        </div>
-        <div className="title">
-          <h3 className="title">Trailer</h3>
-          <div className="trailer-container">
-            {trailer?.embed_url && (
-              <iframe
-                src={trailer?.embed_url}
-                title={title}
-                frameBorder="0"
-                allowFullScreen
-                width="800"
-                height="450"
-                allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              ></iframe>
-            )}
+    <section className="anime-des">
+      <div className="container">
+        <div className="anime-item-box">
+          <h1 className="anime-item-title">{title}</h1>
+          <div className="details">
+            <div className="detail">
+              <div className="anime-image">
+                <img
+                  src={images?.jpg.large_image_url}
+                  alt="Title-image of this anime"
+                ></img>
+              </div>
+              <div className="anime-details">
+                <p className="anime-details-info">
+                  <span className="details-info-prev">Aired: </span>
+                  <span>{aired?.string}</span>
+                </p>
+                <p className="anime-details-info">
+                  <span className="details-info-prev">Rating: </span>
+                  <span>{rating}</span>
+                </p>
+                <p className="anime-details-info">
+                  <span className="details-info-prev">Rank: </span>
+                  <span>{rank}</span>
+                </p>
+                <p className="anime-details-info">
+                  <span className="details-info-prev">Scored by: </span>
+                  <span>{score}</span>
+                </p>
+                <p className="anime-details-info">
+                  <span className="details-info-prev">Popularity: </span>
+                  <span>{popularity}</span>
+                </p>
+                <p className="anime-details-info">
+                  <span className="details-info-prev">Status: </span>
+                  <span>{status}</span>
+                </p>
+                <p className="anime-details-info">
+                  <span className="details-info-prev">Source: </span>
+                  <span>{source}</span>
+                </p>
+                <p className="anime-details-info">
+                  <span className="details-info-prev">Season: </span>
+                  <span>{season}</span>
+                </p>
+                <p className="anime-details-info">
+                  <span className="details-info-prev">Duration: </span>
+                  <span>{duration}</span>
+                </p>
+              </div>
+            </div>
+            <div className="descriptio-box">
+              <h4 className="description-title">description</h4>
+              <p className="description">
+                {showMore ? synopsis : synopsis?.substring(0, 200) + "..."}
+              </p>
+              <button
+                className="description-btn"
+                onClick={() => {
+                  setShowMore(!showMore);
+                }}
+              >
+                {showMore ? "Close" : "reade more"}
+              </button>
+            </div>
+            <div className="trailer">
+              <h3 className="trailer-title">Trailer</h3>
+              <div className="trailer-container">
+                {trailer?.embed_url && (
+                  <iframe
+                    src={trailer?.embed_url}
+                    title={title}
+                    frameBorder="0"
+                    allowFullScreen
+                    width="100%"
+                    height="300"
+                    allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  ></iframe>
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 

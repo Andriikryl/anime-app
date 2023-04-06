1876f7b98058ab215f24ff2f07bbc76fdb3fc0c9
Andriikryl <andreyk557@gmail.com>
2023-04-06 11:42:21 +0300

    feat[global-context]

---
+++ src/context/global.js
@@ -0,0 +1,9 @@
+import React, { createContext } from "react";
+
+const GlobalContext = createContext();
+
+export const GlobalContextProvider = ({ children }) => {
+  return (
+    <GlobalContext.Provider value={"hello"}>{children}</GlobalContext.Provider>
+  );
+};

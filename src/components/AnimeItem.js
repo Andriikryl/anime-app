529f247939d414961ac1b25e9b49988fda7dc28e
Andriikryl <andreyk557@gmail.com>
2023-04-06 14:22:38 +0300

    feat[anime-info-destruction]

--- src/components/AnimeItem.js
+++ src/components/AnimeItem.js
@@ -9,6 +9,25 @@ function AnimeItem() {
   const [characters, setCharacters] = React.useState([]);
   const [showMore, setShowMore] = React.useState(false);
 
+  //destructer anime
+
+  const {
+    title,
+    synopsis,
+    trailer,
+    duration,
+    aired,
+    season,
+    images,
+    rank,
+    score,
+    scored_by,
+    popularity,
+    status,
+    rating,
+    source,
+  } = anime;
+
   //get anime based on id
 
   const getAnime = async (anime) => {
@@ -22,7 +41,84 @@ function AnimeItem() {
     getAnime(id);
   }, []);
 
-  return <div>AnimeItem</div>;
+  return (
+    <div>
+      <h1>{title}</h1>
+      <div className="details">
+        <div className="detail">
+          <div className="image">
+            <img
+              src={images?.jpg.large_image_url}
+              alt="Title-image of this anime"
+            ></img>
+          </div>
+          <div className="anime-details">
+            <p>
+              <span>Aired: </span>
+              <span>{aired?.string}</span>
+            </p>
+            <p>
+              <span>Rating: </span>
+              <span>{rating}</span>
+            </p>
+            <p>
+              <span>Rank: </span>
+              <span>{rank}</span>
+            </p>
+            <p>
+              <span>Scored by: </span>
+              <span>{score}</span>
+            </p>
+            <p>
+              <span>Popularity: </span>
+              <span>{popularity}</span>
+            </p>
+            <p>
+              <span>Status: </span>
+              <span>{status}</span>
+            </p>
+            <p>
+              <span>Source: </span>
+              <span>{source}</span>
+            </p>
+            <p>
+              <span>Season: </span>
+              <span>{season}</span>
+            </p>
+            <p>
+              <span>Duration: </span>
+              <span>{duration}</span>
+            </p>
+          </div>
+        </div>
+        <div>
+          <p className="description">
+            {showMore ? synopsis : synopsis?.substring(0, 200) + "..."}
+          </p>
+          <button
+            onClick={() => {
+              setShowMore(!showMore);
+            }}
+          >
+            {showMore ? "Close" : "reade more"}
+          </button>
+        </div>
+        <div className="title">
+          <h3 className="title">Trailer</h3>
+          <div className="trailer-container">
+            {trailer?.embed_url && (
+              <iframe
+                src={trailer?.embed_url}
+                title={title}
+                frameBorder="0"
+                allowFullScreen
+              ></iframe>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AnimeItem;
